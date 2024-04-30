4635dc1e37b043c0e7878e85e3dc07d28e774d56
Tech13-08 <falaktulsi@gmail.com>
2024-04-30 01:20:20 -0700

    removed routing for now

--- src/main.jsx
+++ src/main.jsx
@@ -2,25 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./pages/App.jsx";
 import "./index.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { TerminalContextProvider } from "react-terminal";
-import { Layout } from "./pages/Layout.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route
-            index
-            element={
-              <TerminalContextProvider>
-                <App />
-              </TerminalContextProvider>
-            }
-          />
-        </Route>
-      </Routes>
+      <TerminalContextProvider>
+        <App />
+      </TerminalContextProvider>
     </Router>
   </React.StrictMode>
 );
